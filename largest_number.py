abcf30ab3a00d09cd8f42bebd6769f9586cecac0
aysha12345 <91528182+aysha12345@users.noreply.github.com>
2022-10-10 11:50:58 +0100

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,11 +1,12 @@
-def main():
+def main(ls):
     integer_list = []
-    ls = input("Please enter a list of positive integers (comma separated): ")
+    # ls = input("Please enter a list of positive integers (comma separated): ")
+    ls = ls.replace(" ", "")
     for i in ls.split(","):
-        if i < 0:
+        if int(i) < 0:
             print("Error: Negative integer entered")
             return
-        integer_list.append(i)
+        integer_list.append(int(i))
     return largest_number(integer_list)
 
 def largest_number(i_list):
@@ -24,4 +25,8 @@ def largest_number(i_list):
         output = output + str(list_of_lists[i][-1])
     return int(output)
 
+        
+     
+    
+            
         
