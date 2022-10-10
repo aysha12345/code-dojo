c835d52fcad8cd0a646e774a820d9bb3b5e01577
aysha12345 <91528182+aysha12345@users.noreply.github.com>
2022-10-10 11:51:12 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
-from ex1 import largest_number
+from ex1 import largest_number, main
 
 def test_largest_number():
     assert largest_number([50, 2, 1, 9]) == 95021
     assert largest_number([5, 50, 56]) == 56550
     assert largest_number([420, 42, 423]) == 42423420
+    
+def test_main():
+    assert main("50, 2, 1, 9") == 95021
+    assert main("23, 67, 2, 9") == 967232
     
